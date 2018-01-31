16e018ecb958e94a8b052a286df51da592032354
Md Zahid Raza <zahid7292@ymail.com>
2018-01-31 22:11:21 +0530

    first commmit

---
+++ test1/src/main/java/com/jazasoft/test/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.jazasoft.test;
+
+/**
+ * @author Md Zahid Raza
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+}
